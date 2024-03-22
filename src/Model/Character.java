34d89b430c6427396b97c80763e1d8fc3cb0f4ce
Kurely Mózes <mozes.kurely@gmail.com>
2024-03-22 17:40:39 +0100

    Character, Teacher, Student osztályok

--- src/Model/Character.java
+++ src/Model/Character.java
@@ -1,5 +1,48 @@
 package Model;
 
-public class Character {
-    
+import java.util.List;
+
+public abstract class Character {
+    String name;
+    List<Item> inventory;
+    Room location;
+    bool alive;
+    List<Item> activatedItems;
+public
+    Character(){
+        System.out.println("Function: Character + Constructor");
+    }
+    bool pickUpItem(Item i){
+        System.out.println("Function: Character + pickUpItem");
+        return true;
+    }
+    void dropItem(Item i){
+        System.out.println("Function: Character + dropItem");
+    }
+    String getName(){
+        System.out.println("Function: Character + getName");
+        return "";
+    }
+    Room getLocation(){
+        System.out.println("Function: Character + getLocation");
+        return null;
+    }
+    void dropAllItem(){
+        System.out.println("Function: Character + dropAllItem");
+    }
+    bool gasAttack(){
+        System.out.println("Function: Character + gasAttack");
+        return true;
+    }
+    bool hasTheSlideRule(){
+        System.out.println("Function: Character + hasTheSlideRule");
+        return true;
+    }
+    bool isAlive(){
+        System.out.println("Function: Character + isAlive");
+        return true;
+    }
+    abstract bool teacherAttack();
+
+    abstract bool  ragAttack();
 }
--- src/Model/Teacher.java
+++ src/Model/Teacher.java
@@ -1,5 +1,22 @@
 package Model;
 
 public class Teacher {
-    
+   
+public
+    Teacher(){
+        System.out.println("Function: Teacher + Konstructor");
+    }
+     void tryToKill(){
+        System.out.println("Function: Teacher + tryToKill");
+     }
+     bool teacherAttack(){
+        System.out.println("Function: Teacher + teacherAttack");
+        return false;
+     }
+     bool ragAttack(){
+        System.out.println("Function: Teacher + ragAttack");
+        return true;
+     }
+
+
 }
