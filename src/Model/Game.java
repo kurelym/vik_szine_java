152308c64865a606c711ce4b16167f5ed5e6b525
Kántor Kristóf Zoltán <kantor.kristof5@gmail.com>
2024-03-23 12:22:53 +0100

    Skeleton 1.2

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -5,67 +5,67 @@ package Model;
 public class Game {
 
     public Game() {
-        System.out.println("Function: [Game] consturctor");
+        System.out.println("Function: Game class consturctor");
     }
 
     // @return az aktualis kor szama
     public int getRound() {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+        System.out.println("Function: Game class + getRound()");
         return 1;
     }
 
     // Elinditja a jatekot (inicializal)
     public void startGame() {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+        System.out.println("Function: Game class + startGame()");
     }
 
     // Ellenorzi, hogy valamely hallgato felvette-e a logarlecet
     public void win() {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+        System.out.println("Function: Game class + win()");
     }
 
     // Noveli a kor szamat
     public void incrementRound() {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+        System.out.println("Function: Game class + incrementRound()");
     }
 
     // Csokken az erteke, ha egy jatekos meghal
     public void decreasePlayerNumber() {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+        System.out.println("Function: Game class + decreasePlayerNumber()");
     }
 
     // Hallgato hozzaadasa a jatekhoz
     // @param Felvett hallgato
     public void addStudent(Student s) {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+        System.out.println("Function: Game class + addStudent()");
     }
 
     // Oktato hozzaadasa a jatekhoz
     // @param Felvett oktato
     public void addTeacher(Teacher t) {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+        System.out.println("Function: Game class + addTeacher()");
     }
 
     // Hallgato elvetele a jatekbol
     // @param Elvett hallgato
-    void removeStudent(Student s) {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+    public void removeStudent(Student s) {
+        System.out.println("Function: Game class + removeStudent()");
     }
 
     // Oktato elvetele a jatekbol
     // @param Elvett oktato
-    void removeTeacher(Teacher t) {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+    public void removeTeacher(Teacher t) {
+        System.out.println("Function: Game class + removeTeacher()");
     }
 
     // A jatek elejen a labirintust epiti fel
-    void initConnectRooms() {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+    public void initConnectRooms() {
+        System.out.println("Function: Game class + initConnectRooms()");
     }
 
     // A korok vegen modositja a labirintust
-    void manipulateRooms() {
-        System.out.println("Function: [Game] " + getClass().getEnclosingMethod().getName());
+    public void manipulateRooms() {
+        System.out.println("Function: Game class + manipulateRooms()");
     }
 
 }
