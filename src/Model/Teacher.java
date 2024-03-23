4f8adfa533e289e354a61a3ab7602e996d368c5e
Kurely Mózes <mozes.kurely@gmail.com>
2024-03-23 12:40:51 +0100

    JavaDoc

--- src/Model/Teacher.java
+++ src/Model/Teacher.java
@@ -1,20 +1,39 @@
 package Model;
 
-public class Teacher {
-   
-   public Teacher(){
-        System.out.println("Function: Teacher class + Constructor func");
-    }
-     public void tryToKill(){
-        System.out.println("Function: Teacher class + tryToKill func");
-     }
-     public boolean teacherAttack(){
-        System.out.println("Function: Teacher class + teacherAttack func");
-        return false;
-     }
-     public boolean ragAttack(){
-        System.out.println("Function: Teacher class + ragAttack func");
-        return true;
-     }
+/**
+ * Az osztály egy oktatót reprezentál a játékban.
+ */
+public class Teacher extends Character {
 
+    /**
+     * Konstruktor a Teacher osztályhoz.
+     */
+    public Teacher(){
+        System.out.println("Függvény: Teacher osztály + Konstruktor függvény");
+    }
+
+    /**
+     * Metódus amely levezérli az oktató támadási próbálkozását abban a szobában ahol tartózkodik
+     */
+    public void tryToKill(){
+        System.out.println("Függvény: Teacher osztály + tryToKill függvény");
+    }
+
+    /**
+     * Metódus a tanár támadás kezelésére a karakteren.
+     * @return Mindig false, mivel az oktatók támadása nincs hatással egy másik oktatóra
+     */
+    public boolean teacherAttack(){
+        System.out.println("Függvény: Teacher osztály + teacherAttack függvény");
+        return false;
+    }
+
+    /**
+     * Metódus a nedves táblatörlő támadás kezelésére a karakteren.
+     * @return Mindig true, mivel a nedves táblatörlő mindig hatásos az oktatók ellen.
+     */
+    public boolean ragAttack(){
+        System.out.println("Függvény: Teacher osztály + ragAttack függvény");
+        return true;
+    }
 }
